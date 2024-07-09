088cefb8dcdfc088b8741c37475a6b5dfb9b673f
omerdr7 <omerfarukdere32@gmail.com>
2024-07-09 22:03:57 +0300

    MyFrame class created.It extend jframe class.

--- src/movingobjects/MyFrame.java
+++ src/movingobjects/MyFrame.java
@@ -4,4 +4,14 @@ import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
 
+    Panel panel;
+
+    public MyFrame() {
+        panel = new Panel();
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }

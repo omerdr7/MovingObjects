2334bddca1c6dd04da3c15621935369457b53985
omerdr7 <omerfarukdere32@gmail.com>
2024-07-09 22:05:36 +0300

    Required variables are defined.

--- src/movingobjects/Panel.java
+++ src/movingobjects/Panel.java
@@ -1,7 +1,18 @@
 package movingobjects;
 
+import java.awt.Image;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Panel extends JPanel {
 
+    final int PANEL_WİTDH = 900;
+    final int PANEL_HEİGHT = 630;
+    int x = 0;
+    int y = 0;
+    int xVelocity = 2;
+    int yVelocity = 1;
+    Image bird;
+    Image backgroundImage;
+    Timer timer;
 }
